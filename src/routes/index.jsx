d16f3add2b1d597379a35919ce7b6652aa0b9c1d
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-04 12:30:55 -0400

    :sparkles: feat: Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/LadingPage";
+import { Portifolio } from "../shared/components/Hero_Projects";
+import { Hero } from "../shared/components/Hero";
+import { Hero_Skills } from "../shared/components/Hero_Skills";
+import { Hero_SoftSkills } from "../shared/components/Hero-SoftSkills";
+import { Footer } from "../shared/components/Footer";
 
 export const Rotas = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <Hero />
+            <Hero_Skills />
+            <Hero_SoftSkills />
+            <Footer />
+          </>
+        ),
+      },
+      {
+        path: "/projects",
+        element: (
+          <>
+            <Portifolio />
+          </>
+        ),
+      },
+    ],
   },
 ]);
