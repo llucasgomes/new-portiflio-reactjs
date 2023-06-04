dad217251bea9fb31564e2d7daca7651f29b40b3
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-04 18:21:27 -0400

    :lipstick: feat: Estilizaçao Styled Components

--- src/shared/components/Hero_Projects/styled.js
+++ src/shared/components/Hero_Projects/styled.js
@@ -1,45 +1,9 @@
 import styled from "styled-components";
 
-export const C_Projects = styled.section`
-  width: 100vw;
-  height: calc(100vh - 5rem);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem 0;
-  gap: 1rem;
-  background-color: #525e68;
-
-  .container {
-    max-width: 80%;
-    margin: 0 auto;
-
-    background-color: red;
-  }
-  .carrousel {
-    cursor: grab;
-    overflow: hidden;
-  }
-  .inner {
-    display: flex;
-  }
-  .item {
-    min-height: 200px;
-    min-width: 400px;
-    padding: 14px;
-  }
-  .item img {
-    width: 100%;
-    height: 90%;
-    border-radius: 12px;
-    pointer-events: none;
-  }
-`;
-
 export const Container_Portifolio = styled.div`
   width: 100%;
 
-  padding: 2rem;
+  padding: 6rem 2rem 2rem 2rem;
 
   .title {
     width: 100%;
