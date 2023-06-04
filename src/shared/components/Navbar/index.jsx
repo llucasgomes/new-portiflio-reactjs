e7cab656ed7fe6a6bd6a921f953ac4afbe1889b6
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-04 18:24:31 -0400

    :sparkles: feat: Add handleGo...

--- src/shared/components/Navbar/index.jsx
+++ src/shared/components/Navbar/index.jsx
@@ -1,41 +1,59 @@
 import { Header } from "./styled";
 import Logo from "../../../assets/images/logo.png";
 import { List, X } from "phosphor-react";
-import { useState } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { DataContext } from "../../context/DataContext";
+import { Top } from "../functions";
 
 export const NavBar = () => {
-  const [menu, setMenu] = useState(false);
-  const [navbar, setNavbar] = useState(false);
+  const { navbar, setNavbar, menu_mobile, menu_setMobile } =
+    useContext(DataContext);
 
+  //funccao para scroll trocar Cor
   const changeBgNavbar = () => {
     window.scrollY > 90 ? setNavbar(true) : setNavbar(false);
   };
 
   window.addEventListener("scroll", changeBgNavbar);
 
+  //funcao para fechar o menu e scroll top
+  const handleGo = () => {
+    menu_setMobile(!menu_mobile);
+    Top();
+  };
+
   return (
-    <Header mobile={menu} navbar={navbar}>
+    <Header mobile={menu_mobile} navbar={navbar}>
       <div className="logo">
         <img src={Logo} alt="" />
         <h1>Lucas Gomes</h1>
       </div>
       <div className="mobile-menu">
-        {menu ? (
-          <X className="icon" size={32} onClick={() => setMenu(!menu)} />
-        ) : (
-          <List className="icon" size={32} onClick={() => setMenu(!menu)} />
+        {!menu_mobile && (
+          <List
+            className="icon"
+            size={32}
+            onClick={() => menu_setMobile(!menu_mobile)}
+          />
         )}
       </div>
       <nav>
         <ul className="nav-list">
-          <Link className="item" to={"projects"}>
+          <span className="fechar">
+            <X
+              className="icon"
+              size={32}
+              onClick={() => menu_setMobile(!menu_mobile)}
+            />
+          </span>
+          <Link className="item" to={"projects"} onClick={() => handleGo()}>
             Projetos
           </Link>
-          <Link className="item" to={"skills"}>
+          <Link className="item" to={"skills"} onClick={() => handleGo()}>
             Habilidades
           </Link>
-          <Link className="item" to={"about"}>
+          <Link className="item" to={"about"} onClick={() => handleGo()}>
             Sobre
           </Link>
         </ul>
