accf376c7c78b5e5cbcca0f5a9311747d257d990
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-04 12:30:40 -0400

    :sparkles: feat: Add Routes, Context

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import GlobalStyle from "./styles/Global";
 
 import { RouterProvider } from "react-router-dom";
 import { Rotas } from "./routes";
+import { DataContextProvider } from "./shared/context/DataContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Rotas} />
-
+    <DataContextProvider>
+      <RouterProvider router={Rotas} />
+    </DataContextProvider>
     <GlobalStyle />
   </React.StrictMode>
 );
