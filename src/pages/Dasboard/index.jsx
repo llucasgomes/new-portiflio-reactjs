8e9adf356409c97fb597804890edc7249744c19d
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-29 11:05:59 -0400

    :sparkles::construction: feat:  Dasboard

--- src/pages/Dasboard/index.jsx
+++ src/pages/Dasboard/index.jsx
@@ -1,7 +1,13 @@
+import { D_Habilidades } from "../../shared/components/Dasboard/Habilidades";
+import { Navbar2 } from "../../shared/components/Dasboard/Navbar2";
+import { Container_Dasboard } from "./styled";
+
 export const Dashboard = () => {
   return (
-    <>
-      <h1>Dasboard</h1>
-    </>
+    <Container_Dasboard>
+      <Navbar2 />
+      <D_Habilidades />
+      {/*  <Body_Dashboard /> */}
+    </Container_Dasboard>
   );
 };
---
+++ src/pages/Dasboard/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container_Dasboard = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
