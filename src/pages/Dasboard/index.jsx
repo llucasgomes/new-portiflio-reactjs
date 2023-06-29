dcc6f3c06bd64a9d1441c68945958bf534c8f8d8
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-29 11:26:56 -0400

    :sparkles: feat: Add "Loading"

--- src/pages/Dasboard/index.jsx
+++ src/pages/Dasboard/index.jsx
@@ -1,12 +1,31 @@
 import { D_Habilidades } from "../../shared/components/Dasboard/Habilidades";
 import { Navbar2 } from "../../shared/components/Dasboard/Navbar2";
-import { Container_Dasboard } from "./styled";
+import { Container_Dasboard, Container_Loading } from "./styled";
+import ReactLoading from "react-loading";
 
+import { DataContext } from "../../shared/context/DataContext";
+import { useContext } from "react";
 export const Dashboard = () => {
+  const { isFetching } = useContext(DataContext);
   return (
     <Container_Dasboard>
       <Navbar2 />
-      <D_Habilidades />
+      {isFetching ? (
+        <>
+          <Container_Loading>
+            <ReactLoading
+              type={"spin"}
+              color={"#7279f9"}
+              height={100}
+              width={100}
+            />
+          </Container_Loading>
+        </>
+      ) : (
+        <>
+          <D_Habilidades />
+        </>
+      )}
       {/*  <Body_Dashboard /> */}
     </Container_Dasboard>
   );
--- src/pages/Dasboard/styled.js
+++ src/pages/Dasboard/styled.js
@@ -8,3 +8,12 @@ export const Container_Dasboard = styled.div`
   align-items: center;
   gap: 2rem;
 `;
+
+export const Container_Loading = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f5f5f5;
+`;
